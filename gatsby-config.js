52061b0c254b04e43fc2d7f33fd372fd082b09ce
pt-br <ltavares@yieldstreet.com>
2020-03-21 20:29:38 -0300

    Update metatag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ajuda Corona`,
+    description: `Minere criptomoedas que serão doadas na causa contra a COVID-19.`,
+    author: `@MetflixLabs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
