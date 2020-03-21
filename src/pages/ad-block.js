d41a414d0b028cfea2149d2bd4720c07ab409b2e
pt-br <ltavares@yieldstreet.com>
2020-03-21 20:17:29 -0300

    Update trackings + pageview

--- src/pages/ad-block.js
+++ src/pages/ad-block.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 import Miner from '../components/core/Miner';
@@ -8,81 +8,90 @@ import adblockPlusImg from '../images/adblock-plus.png';
 import adblockImg from '../images/adblock.png';
 import adblockProImg from '../images/adblock-pro.png';
 
-const AdBlock = () => (
-  <Layout>
-    <Miner />
-    <Wrapper>
-      <TextWrapper>
-        <BigText>Seu AdBlock está ligado :(</BigText>
-        <Text>
-          Bloqueadores de anuncio não permitem mineiração de cryptomoedas.
-        </Text>
-        <Content>
-          <Description>
-            Mas calma! Você não precisa remover o seu AdBlock, apenas permitir
-            que ele não bloqueie a sua doação.
-          </Description>
-          <AdBlockWrapper>
-            <AdBlockName>AdBlock Plus</AdBlockName>
-            <AdBlockDescription>
-              1) Clique no ícone do AdBlock Plus, localizado à direita da barra
-              de endereços do seu navegador
-            </AdBlockDescription>
-            <AdBlockDescription>
-              2) Um menu drop-down aparecerá na tela
-            </AdBlockDescription>
-            <AdBlockDescription>
-              3) Clique em “Enabled on this site” para desativar o bloqueio da
-              publicidade
-            </AdBlockDescription>
-            <AdBlockDescription>
-              4) Após o clique, o texto será substituído por “Disabled on this
-              site”
-            </AdBlockDescription>
-            <AdBlockDescription>5) Atualize esta página</AdBlockDescription>
-            <AdBlockImage src={adblockPlusImg} />
-          </AdBlockWrapper>
-          <AdBlockWrapper>
-            <AdBlockName>AdBlock</AdBlockName>
-            <AdBlockDescription>
-              1) Clique no ícone do AdBlock, localizado à direita da barra de
-              endereços do seu navegador
-            </AdBlockDescription>
-            <AdBlockDescription>
-              2) Um menu drop-down aparecerá na tela
-            </AdBlockDescription>
-            <AdBlockDescription>
-              3) Clique em “Don’t run on pages on this domain” para desativar o
-              bloqueio da publicidade
-            </AdBlockDescription>
-            <AdBlockDescription>
-              4) Uma nova janela abrirá e você precisará clicar no botão
-              “Exclude”
-            </AdBlockDescription>
-            <AdBlockDescription>5) Atualize esta página</AdBlockDescription>
-            <AdBlockImage src={adblockImg} />
-          </AdBlockWrapper>
-          <AdBlockWrapper>
-            <AdBlockName>AdBlock Pro</AdBlockName>
-            <AdBlockDescription>
-              1) Clique no ícone do AdBlock Pro, localizado à direita da barra
-              de endereços do seu navegador
-            </AdBlockDescription>
-            <AdBlockDescription>
-              2) Um menu drop-down aparecerá na tela
-            </AdBlockDescription>
-            <AdBlockDescription>
-              3) Clique no primeiro ícone (ligar) para para desativar o bloqueio
-              da publicidade
-            </AdBlockDescription>
-            <AdBlockDescription>4) Atualize esta página</AdBlockDescription>
-            <AdBlockImage src={adblockProImg} />
-          </AdBlockWrapper>
-        </Content>
-      </TextWrapper>
-    </Wrapper>
-  </Layout>
-);
+const AdBlock = () => {
+  useEffect(() => {
+    window.gtag('config', 'UA-161435848-1', {
+      page_title: 'ad-block',
+      page_path: '/ad-block',
+    });
+  }, []);
+
+  return (
+    <Layout>
+      <Miner />
+      <Wrapper>
+        <TextWrapper>
+          <BigText>Seu AdBlock está ligado :(</BigText>
+          <Text>
+            Bloqueadores de anuncio não permitem mineiração de cryptomoedas.
+          </Text>
+          <Content>
+            <Description>
+              Mas calma! Você não precisa remover o seu AdBlock, apenas permitir
+              que ele não bloqueie a sua doação.
+            </Description>
+            <AdBlockWrapper>
+              <AdBlockName>AdBlock Plus</AdBlockName>
+              <AdBlockDescription>
+                1) Clique no ícone do AdBlock Plus, localizado à direita da
+                barra de endereços do seu navegador
+              </AdBlockDescription>
+              <AdBlockDescription>
+                2) Um menu drop-down aparecerá na tela
+              </AdBlockDescription>
+              <AdBlockDescription>
+                3) Clique em “Enabled on this site” para desativar o bloqueio da
+                publicidade
+              </AdBlockDescription>
+              <AdBlockDescription>
+                4) Após o clique, o texto será substituído por “Disabled on this
+                site”
+              </AdBlockDescription>
+              <AdBlockDescription>5) Atualize esta página</AdBlockDescription>
+              <AdBlockImage src={adblockPlusImg} />
+            </AdBlockWrapper>
+            <AdBlockWrapper>
+              <AdBlockName>AdBlock</AdBlockName>
+              <AdBlockDescription>
+                1) Clique no ícone do AdBlock, localizado à direita da barra de
+                endereços do seu navegador
+              </AdBlockDescription>
+              <AdBlockDescription>
+                2) Um menu drop-down aparecerá na tela
+              </AdBlockDescription>
+              <AdBlockDescription>
+                3) Clique em “Don’t run on pages on this domain” para desativar
+                o bloqueio da publicidade
+              </AdBlockDescription>
+              <AdBlockDescription>
+                4) Uma nova janela abrirá e você precisará clicar no botão
+                “Exclude”
+              </AdBlockDescription>
+              <AdBlockDescription>5) Atualize esta página</AdBlockDescription>
+              <AdBlockImage src={adblockImg} />
+            </AdBlockWrapper>
+            <AdBlockWrapper>
+              <AdBlockName>AdBlock Pro</AdBlockName>
+              <AdBlockDescription>
+                1) Clique no ícone do AdBlock Pro, localizado à direita da barra
+                de endereços do seu navegador
+              </AdBlockDescription>
+              <AdBlockDescription>
+                2) Um menu drop-down aparecerá na tela
+              </AdBlockDescription>
+              <AdBlockDescription>
+                3) Clique no primeiro ícone (ligar) para para desativar o
+                bloqueio da publicidade
+              </AdBlockDescription>
+              <AdBlockDescription>4) Atualize esta página</AdBlockDescription>
+              <AdBlockImage src={adblockProImg} />
+            </AdBlockWrapper>
+          </Content>
+        </TextWrapper>
+      </Wrapper>
+    </Layout>
+  );
+};
 
 const Wrapper = styled.div`
   display: block;
