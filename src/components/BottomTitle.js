e118db423fa08226ec4939e2b3db56339dcaea10
eberc <bodacamargo@gmail.com>
2020-03-22 22:14:30 -0300

    separate BottomTitle in component

---
+++ src/components/BottomTitle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const BottomTitle = styled.div`
+  font-size: 36px;
+  line-height: 42px;
+  font-weight: bold;
+  margin-bottom: 15px;
+`;
+
+export default BottomTitle
