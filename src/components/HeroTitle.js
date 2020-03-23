b85df8a05f81cd4fc01ed1334f29322974990614
eberc <bodacamargo@gmail.com>
2020-03-22 22:15:40 -0300

    separate HeroTitle in component

---
+++ src/components/HeroTitle.js
@@ -0,0 +1,11 @@
+import styled from 'styled'
+
+const HeroTitle = styled.div`
+  font-size: 38px;
+  line-height: 56px;
+  font-weight: bold;
+  z-index: 2;
+  position: relative;
+`;
+
+export default HeroTitle
