666cc0c732e5d1de186488b49d570dfbc66d2695
eberc <bodacamargo@gmail.com>
2020-03-22 22:14:46 -0300

    separate CardsWrapper in component

---
+++ src/components/CardsWrapper.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const CardsWrapper = styled.div`
+  display: flex;
+  margin-top: 20px;
+  justify-content: space-between;
+`;
+
+export default CardsWrapper
