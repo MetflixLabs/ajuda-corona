407be379e6963486edcdc62104f2736e58f66b3e
eberc <bodacamargo@gmail.com>
2020-03-22 22:15:29 -0300

    conditional render in Controle

--- src/components/Controle.js
+++ src/components/Controle.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { breakpoints } from '../components/utils/media'
+import useWindowSize from '../hooks/useWindowSize'
 
 import Card from '../components/Card'
 import MineBar from '../components/MineBar';
+import BottomTitle from '../components/BottomTitle'
 
-export default ({currentThrottle, setCurrentThrottle}) => (
-  <Card
-    title="Nível da sua contribuição"
-    description={
-      <MineBar
-        currentThrottle={currentThrottle}
-        setCurrentThrottle={setCurrentThrottle}
-      />
-    }
-    isPurple
-  ></Card>
-)
+export default ({ currentThrottle, setCurrentThrottle }) => {
+
+  const b = breakpoints()
+  const windowSize = useWindowSize()
+  const [isMobile, setIsMobile] = useState(windowSize < b.phoneLandscape);
+
+  useEffect(() => {
+    const width = windowSize.width
+    const vert = width < b.phoneLandscape
+    setIsMobile(vert)
+  }, [windowSize])
+
+  return (
+    <>
+      {isMobile
+        ? (<MineBar
+          currentThrottle={currentThrottle}
+          setCurrentThrottle={setCurrentThrottle}
+        />)
+        : (
+          <>
+          <BottomTitle>Controle</BottomTitle>
+          <Card
+            title="Nível da sua contribuição"
+            description={
+              <MineBar
+                currentThrottle={currentThrottle}
+                setCurrentThrottle={setCurrentThrottle}
+              />
+            }
+            isPurple
+          ></Card>
+          </>)
+      }
+    </>
+
+  )
+}
