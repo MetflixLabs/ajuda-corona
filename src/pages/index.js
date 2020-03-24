c5fa6e92587619792322dcf8569d1b88bb8e9ca3
pt-br <ltavares@yieldstreet.com>
2020-03-24 13:15:24 -0300

    Update country code

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const IndexPage = () => {
       setBrazilData(false);
 
       axios
-        .get('//coronavirus-tracker-api.herokuapp.com/v2/locations/35')
+        .get('//coronavirus-tracker-api.herokuapp.com/v2/locations/28')
         .then(res => {
           const { latest } = res.data.location;
 
