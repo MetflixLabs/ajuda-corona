202bde517f0fbb1f1ccc3801db3896681a7e5d0b
eberc <bodacamargo@gmail.com>
2020-03-23 22:45:08 -0300

    responsive hero title for mobile

--- src/components/HeroTitle.js
+++ src/components/HeroTitle.js
@@ -1,11 +1,19 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import media from '@utils/media';
 
 const HeroTitle = styled.div`
-  font-size: 38px;
-  line-height: 56px;
-  font-weight: bold;
   z-index: 2;
   position: relative;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 32px;
+  line-height: 37px;
+
+  ${media.phoneLandscape`
+    font-size: 38px;
+    line-height: 56px;
+    font-weight: bold;
+  `}
 `;
 
-export default HeroTitle
+export default HeroTitle;
