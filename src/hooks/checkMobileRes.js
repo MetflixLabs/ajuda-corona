47fc432cf3e380c47b055c32f5683a1df12cf817
eberc <bodacamargo@gmail.com>
2020-03-23 22:44:21 -0300

    add: new hook to check mobile res

---
+++ src/hooks/checkMobileRes.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+import useWindowSize from '@hooks/useWindowSize';
+import { breakpoints } from '@utils/media';
+
+export default () => {
+  const windowSize = useWindowSize();
+  const b = breakpoints();
+  const [isMobile, setIsMobile] = useState(windowSize < b.phoneLandscape);
+
+  useEffect(() => {
+    const isMobile = windowSize.width < b.phoneLandscape;
+    setIsMobile(isMobile);
+  }, [windowSize]);
+
+  return isMobile;
+};
